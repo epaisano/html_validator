129775440480b6359bfb2a0b26d41dc19db342ad
Emma Munoz <epaisano24@cmc.edu>
2023-02-07 10:30:41 -0800

    fixed long line

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -24,8 +24,8 @@ def validate_html(html):
         else:
             if len(stack) == 0:
                 return False
-            if thtml[tag][:(-len(stack[-1]))
-                          + 1:-1] == stack[-1][:(-len(stack[-1])) + 1:-1]:
+            tmp=(-len(stack[-1])) + 1
+            if thtml[tag][:tmp:-1] == stack[-1][:tmp:-1]:
                 stack.pop()
             else:
                 return False
