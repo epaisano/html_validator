5c23b0d9e83dd230bdcd14cf43737fa8e1ab1806
Emma Munoz <epaisano24@cmc.edu>
2023-02-07 09:44:53 -0800

    fixed linter

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,31 +1,45 @@
 #!/bin/python3
 
 
+import re
+
+
 def validate_html(html):
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
+    This function performs a limited version of html validation
+    by checking whether every opening tag has a corresponding closing tag.
 
     >>> validate_html('<strong>example</strong>')
     True
     >>> validate_html('<strong>example')
     False
     '''
-
-    # HINT:
-    # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the class/book
-    # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
-    # but arbitrary text located between the html tags
+    thtml = _extract_tags(html)
+    stack = []
+    if len(html) > 0 and len(thtml) <= 1:
+        return False
+    for tag in range(len(thtml)):
+        if "/" not in thtml[tag]:
+            stack.append(thtml[tag])
+        else:
+            if len(stack) == 0:
+                return False
+            if thtml[tag][:(-len(stack[-1]))
+                          + 1:-1] == stack[-1][:(-len(stack[-1])) + 1:-1]:
+                stack.pop()
+            else:
+                return False
+    return (len(stack) == 0)
 
 
 def _extract_tags(html):
     '''
-    This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
-
-    This function returns a list of all the html tags contained in the input string,
-    stripping out all text not contained within angle brackets.
+    This function returns a list of all the html tags
+    contained in the input string, stripping out all
+    text not contained within angle brackets.
 
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+    html = re.findall("<([^ >]+)", html)
+    return list(map(lambda tag: "<" + tag + ">", html))
